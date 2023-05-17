504ea4c555e9d6eecc1658418e19e4577e1f1250
gauravkale2799 <gauravkale2799@gmail.com>
2023-05-17 14:06:26 +0200

    initail setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import {useEffect,useState,useContext} from "react";
+import {useNavigate} from "react-router-dom";
+
+
+
+import "./Header.scss";
+const Header = () => {
+    return <div>Header</div>;
+};
+
+export default Header;
