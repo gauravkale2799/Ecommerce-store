bded8099eaed6e7c0bb95f8a4b503a85f96dc3b0
gauravkale2799 <gauravkale2799@gmail.com>
2023-05-19 09:06:44 +0200

    Header Part Completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,53 @@
-import {useEffect,useState,useContext} from "react";
-import {useNavigate} from "react-router-dom";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import React,{useEffect} from "react";
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
 
-import {TbSearch} from "react-icons/tb";
-import {CgShoppingCart} from "react-icons/cg"
-import {AiOutlineHeart} from "react-icons/ai"
-
-import Search  from "./Search/Search";
+import Search from "./Search/Search";
 import Cart from "../Cart/Cart";
 import { Context } from "../../utils/context";
 
 import "./Header.scss";
+// use state is used to set the value of the data manually
+//useEffect is used for the component to render when it call first time,
+// dependencies array in state , state update when the useEffect call it
 const Header = () => {
-    return <header className="main-header">
-        <div className="header-content">
-            <ul className="left">
-                <li>Home</li>
-                <li>About</li>
-                <li>Categories</li>
-            </ul>
-            <div className="centre">GKDEVSTORE.</div>
-            <div className="right">
-                <TbSearch/>
-                <AiOutlineHeart/>
-                <span className="cart-icon">
-                    <CgShoppingCart/>
-                </span>
-                
-            </div>
-        
+  const [scrolled, setScrolled] = React.useState(false);
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    }
+     else {
+      setScrolled(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+  }, []);
+  // back tic can be used to write your own java script code
+  return (
+    <header className={`main-header${scrolled ? 'stickey-header' : ""}`}>
+      <div className="header-content">
+        <ul className="left">
+          <li>Home</li>
+          <li>About</li>
+          <li>Categories</li>
+        </ul>
+        <div className="centre">GKDEVSTORE.</div>
+        <div className="right">
+          <TbSearch />
+          <AiOutlineHeart />
+          <span className="cart-icon">
+            <CgShoppingCart />
+            <span>5</span>
+          </span>
         </div>
-
+      </div>
     </header>
+  );
 };
 
 export default Header;
