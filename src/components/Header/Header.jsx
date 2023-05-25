d21ffe55c9d4a5ac79916d4cfde99565973dd54a
gauravkale2799 <gauravkale2799@gmail.com>
2023-05-26 01:40:14 +0200

    Adding stickey Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,32 @@
-import { useState, useContext, useEffect } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import React from "react";
 import { TbSearch } from "react-icons/tb";
 import { CgShoppingCart } from "react-icons/cg";
 import { AiOutlineHeart } from "react-icons/ai";
-
-import Search from "./Search/Search";
-import Cart from "../Cart/Cart";
-import { Context } from "../../utils/context";
-
 import "./Header.scss";
-// use state is used to set the value of the data manually
-//useEffect is used for the component to render when it call first time,
-// dependencies array in state , state update when the useEffect call it
+import Search from "./Search/Search";
+import { Context } from "../../utils/context";
+import Cart from "../Cart/Cart";
+// back tic can be used to write your own java script code
 const Header = () => {
-  const [scrolled, setScrolled] = React.useState(false);
-  const handleScroll = () => {
+  const [scrolled, setScrolled] = useState(false);
+  const handleScroll = ()=>{
     const offset = window.scrollY;
-    if (offset > 200) {
+    if(offset > 200){
       setScrolled(true);
-    }
-     else {
+    } else{
       setScrolled(false);
     }
+      
   };
-  useEffect(() => {
+  useEffect(()=> {
     window.addEventListener("scroll", handleScroll)
+      
   }, []);
-  // back tic can be used to write your own java script code
+    
   return (
-    <header className={`main-header${scrolled ? 'stickey-header' : ""}`}>
+    <>
+      <header className={`main-header ${scrolled ? "stickey-header" : ""}`}>
       <div className="header-content">
         <ul className="left">
           <li>Home</li>
@@ -47,7 +44,12 @@ const Header = () => {
         </div>
       </div>
     </header>
+  </>
   );
 };
 
 export default Header;
+
+// use state is used to set the value of the data manually
+//useEffect is used for the component to render when it call first time,
+// dependencies array in state , state update when the useEffect call it
