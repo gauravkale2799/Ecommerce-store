88430da2b9c73654567eea280deaa14e4680dc6e
gauravkale2799 <gauravkale2799@gmail.com>
2023-05-26 13:09:52 +0200

    News Letter Jsx Completed

--- src/components/Footer/Newsletter/Newsletter.jsx
+++ src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,6 +1,39 @@
+import React from "react";
+import {
+    FaFacebookF,
+    FaTwitter,
+    FaInstagram,
+    FaLinkedinIn,
+} from "react-icons/fa";
 import "./Newsletter.scss";
 const Newsletter = () => {
-    return <div>Newsletter</div>;
+    return <div className="newsletter-section">
+        <div className="newsletter-content">
+            <span className="small-text">News Letter</span>
+            <span className="big-text">
+                Sign up for the latest updates and offers
+            </span>
+            <div className="form">
+                <input type="text" placeholder="Email Address" />
+                <button>Subcribe</button>
+            </div>
+            <div className="text">Will be used in accordance with our Privacy Policy</div>
+            <div className="social-icons">
+                <div className="icon">
+                    <FaFacebookF size={14}/>
+                </div>
+                <div className="icon">
+                    <FaTwitter size={14}/>
+                </div>
+                <div className="icon">
+                    <FaInstagram size={14}/>
+                </div>
+                <div className="icon">
+                    <FaLinkedinIn size={14}/>
+                </div>
+            </div>
+        </div>
+    </div>;
 };
 
 export default Newsletter;
