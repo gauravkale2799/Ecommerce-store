3cc0ba4827a2d5acdaa2feca6e7ed3c741944665
gauravkale2799 <gauravkale2799@gmail.com>
2023-05-30 00:29:51 +0200

    All HTMl, Css code - template completed

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,6 +1,12 @@
 import "./Category.scss";
+import Products from "../Products/Products"
 const Category = () => {
-    return <div>Category</div>;
-};
+    return <div className="category-main-content">
+        <div className="layput">
+            <div className="category-title">Category Title</div>
+            <Products innerPage ={true}/>
 
+        </div>
+    </div>
+}
 export default Category;
--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,67 @@
+// import { useContext, useState } from "react";
+// import { Context } from "../../utils/context";
+// import { useParams } from "react-router-dom";
+// import useFetch from "../../hooks/useFetch";
+import RelatedProducts from "./RelatedProducts/RelatedProducts";
+import prod from "../../assets/products/earbuds-prod-1.webp";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+  FaPinterest,
+  FaCartPlus,
+} from "react-icons/fa";
 import "./SingleProduct.scss";
+
 const SingleProduct = () => {
-    return <div>Single Product</div>;
+  return (
+    <div className="single-product-main-content">
+      <div className="layout">
+        <div className="single-product-page">
+          <div className="left">
+            <img src={prod} />
+          </div>
+          <div className="right">
+            <span className="name">Product name</span>
+            <span className="price">productprice</span>
+            <span className="desc">product.description</span>
+
+            <div className="cart-buttons">
+              <div className="quantity-buttons">
+                <span>-</span>
+                <span>5</span>
+                <span>+</span>
+              </div>
+              <button className="add-to-cart-button">
+                <FaCartPlus size={20} />
+                ADD TO CART
+              </button>
+            </div>
+
+            <span className="divider" />
+            <div className="info-item">
+              <span className="text-bold">
+                Category:
+                <span>Headphone</span>
+              </span>
+              <span className="text-bold">
+                Share:
+                <span className="social-icons">
+                  <FaFacebookF size={16} />
+                  <FaTwitter size={16} />
+                  <FaInstagram size={16} />
+                  <FaLinkedinIn size={16} />
+                  <FaPinterest size={16} />
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <RelatedProducts />
+      </div>
+    </div>
+  );
 };
 
 export default SingleProduct;
